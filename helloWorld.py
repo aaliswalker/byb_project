304fde4e04345013a2b7490b869df0a175a9ce76
Praise <praise2205@gmail.com>
2024-03-11 22:05:26 +0000

    Added milk liters input

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,8 @@
 print("Git is awesome!")
 user_input = input("Enter something: ")
 print("You entered:", user_input)
+
+print("Just adding some changes")
+print(":)")
+
+liters_milk = int(input("How many liters of milk did you drink today: "))
